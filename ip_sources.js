8bde49325a872f04391b950eaa39289314294b2c
sever158 <windows18@139.com>
2025-06-08 13:10:08 +0800

    Update ip_sources.js

--- ip_sources.js
+++ ip_sources.js
@@ -1,7 +1,4 @@
 export default [
-  'https://api.proxyscrape.com/v2/?request=getproxies&protocol=http',
-  'https://www.proxy-list.download/api/v1/get?type=http',
-  'https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt',
   'https://raw.githubusercontent.com/ymyuuu/IPDB/refs/heads/main/BestProxy/proxy.txt',
   'https://checkerproxy.net/api/archive/2025-06-07#HTTP',
   'https://checkerproxy.net/api/archive/2025-06-07#HTTPS'
