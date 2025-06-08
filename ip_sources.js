63156e26874538ad928aae5398ea838efe924160
sever158 <windows18@139.com>
2025-06-08 13:16:49 +0800

    Update ip_sources.js

--- ip_sources.js
+++ ip_sources.js
@@ -1,5 +1,4 @@
 export default [
-  'https://raw.githubusercontent.com/ymyuuu/IPDB/refs/heads/main/BestProxy/proxy.txt',
   'https://checkerproxy.net/api/archive/2025-06-07#HTTP',
   'https://checkerproxy.net/api/archive/2025-06-07#HTTPS'
 ];
